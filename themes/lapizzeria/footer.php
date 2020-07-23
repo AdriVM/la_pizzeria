c6e43b5b541d107ab184cfedf2dae92f792238cf
Adrián <avazmir1337@gmial.com>
2020-07-23 14:29:14 +0200

    Creación del footer

--- themes/lapizzeria/footer.php
+++ themes/lapizzeria/footer.php
@@ -1,3 +1,28 @@
-</body>
+    <footer class="site-footer">
+        <?php 
 
-</html>
+            $args =  array(
+                'theme_location' => 'header-menu',
+                'container' => 'nav',
+                'container_class' => 'footer-nav',
+                'after' => '<span class="separador"> | </span>'
+
+
+            );
+
+            wp_nav_menu( $args );
+
+        ?>
+        <div class="direccion">
+            <p>
+                Calle Valmojado, 215, 28047 Madrid
+            </p>
+            <p>
+                Teléfono: 649875130
+            </p>
+        </div><!-- FIN .direccion -->
+    </footer>
+    <?php wp_footer(); ?>
+    </body>
+
+    </html>
